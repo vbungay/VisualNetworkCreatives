963da987ecd5d0d8f9d9542df7111b7840abff19
vbungay <113390913+vbungay@users.noreply.github.com>
2023-06-07 10:53:20 -0400

    Animations

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+import { motion } from 'framer-motion'
 import { BsArrowUpRight } from 'react-icons/bs'
 import bgVid from '../../assets/vid/bgVid.mp4'
 
 const Home = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0); // Scroll to the top of the page on route change
+    }, []);
+
+    const wipeDown = {
+        initial: { scaleY: 0, originY: -1 },
+        animate: { scaleY: 1, transition: { duration: .5, delay: 3 } },
+    };
+
+    const webDesignVariants = {
+        hidden: { opacity: 0, x: -20 },
+        visible: { opacity: 1, x: 0, transition: { duration: 1, delay: 2.5 } }
+    };
+
+    const wipeUp = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0, transition: { duration: 1, delay: 3 } }
+    };
+
     return (
         <div className="relative w-full h-screen overflow-hidden">
             <video
@@ -15,25 +35,39 @@ const Home = () => {
                 <source src={bgVid} type="video/mp4" />
             </video>
             <div className="absolute inset-0 flex flex-col items-start justify-evenly pl-4 lg:pl-20">
-                <div className="pt-32">
+                <motion.div
+                    className="pt-32"
+                    initial="hidden"
+                    animate="visible"
+                    variants={webDesignVariants}
+                >
                     <div className="flex flex-col lg:flex-row gap-0 lg:gap-5">
                         <h1 className="text-4xl md:text-7xl font-bold uppercase text-white font-monument lg:leading-[5rem]">Visual</h1>
                         <h1 className="text-4xl md:text-7xl font-bold uppercase text-white font-monument lg:leading-[5rem]">Network</h1>
                     </div>
 
                     <h1 className="text-4xl md:text-7xl font-bold uppercase text-white font-monument tracking-widest">Creatives</h1>
-                </div>
+                </motion.div>
                 <div className="">
-                    <p className="text-white">
+                    <motion.p
+                        className="text-white"
+                        initial={wipeDown.initial}
+                        animate={wipeDown.animate}
+                    >
                         Embrace the power of visual connectivity, let us transform your online presence into a captivating network of creativity, functionality, and success!
-                    </p>
-                    <div className="flex items-center gap-2 pt-4 cursor-pointer">
+                    </motion.p>
+                    <motion.div
+                        className="flex items-center gap-2 pt-4 cursor-pointer"
+                        initial="hidden"
+                        animate="visible"
+                        variants={wipeUp}
+                    >
                         <p className="uppercase text-2xl underline underline-offset-4">
-                        Get in touch
-                    </p>
-                    <BsArrowUpRight className="text-2xl" />
-                    </div>
-                    
+                            Get in touch
+                        </p>
+                        <BsArrowUpRight className="text-2xl" />
+                    </motion.div>
+
                 </div>
             </div>
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,37 +8,94 @@ const Navbar = () => {
     const toggleMenu = () => {
         setShowMenu(!showMenu);
     };
+
+    const wipeRightToLeft = {
+        initial: { scaleX: 0, originX: 1 },
+        animate: { scaleX: 1, transition: { duration: 1, delay: .5 } },
+    };
+
+    const webDesignVariants = {
+        hidden: { opacity: 0, x: -50 },
+        visible: { opacity: 1, x: 0, transition: { duration: 0.5, delay: 2 } }
+    };
+
+    const graphicsVariants = {
+        hidden: { opacity: 0, x: 50 },
+        visible: { opacity: 1, x: 0, transition: { duration: 0.5, delay: 2 } }
+    };
+
+    const wipeDown = {
+        initial: { scaleY: 0, originY: -1 },
+        animate: { scaleY: 1, transition: { duration: 0.5, delay: 1.5 } },
+    };
+
     return (
         <div className="w-full fixed z-10 overflow-x-hidden">
-            <div className="flex justify-between text-black border-b border-white items-center">
-                <span className="pl-4 pt-2 pb-2 lg:pl-20 lg:pt-5 lg:pb-5 font-monument tracking-widest">
+            <motion.div
+                className="flex justify-between text-black border-b border-white items-center"
+                initial={wipeRightToLeft.initial}
+                animate={wipeRightToLeft.animate}
+            >
+                <motion.span
+                    className="pl-4 pt-2 pb-2 lg:pl-20 lg:pt-5 lg:pb-5 font-monument tracking-widest"
+                    initial="hidden"
+                    animate="visible"
+                    variants={webDesignVariants}
+                >
                     vnc
-                </span>
-                <div className="flex gap-5 border-l border-white pl-4 pr-4 pt-2 pb-2 lg:pt-5 lg:pb-5 lg:pl-20 lg:pr-20">
-                    <span className={`hidden lg:inline cursor-pointer ${showMenu ? 'hidden' : ''}`}>
+                </motion.span>
+                <motion.div
+                    className="flex gap-5 border-l border-white pl-4 pr-4 pt-2 pb-2 lg:pt-5 lg:pb-5 lg:pl-20 lg:pr-20"
+                    initial={wipeDown.initial}
+                    animate={wipeDown.animate}
+                >
+                    <motion.span
+                        className={`hidden lg:inline cursor-pointer ${showMenu ? 'hidden' : ''}`}
+                        initial="hidden"
+                        animate="visible"
+                        variants={graphicsVariants}
+                    >
                         About
-                    </span>
-                    <span className={`hidden lg:inline cursor-pointer ${showMenu ? 'hidden' : ''}`}>
+                    </motion.span>
+                    <motion.span
+                        className={`hidden lg:inline cursor-pointer ${showMenu ? 'hidden' : ''}`}
+                        initial="hidden"
+                        animate="visible"
+                        variants={graphicsVariants}
+                    >
                         Works
-                    </span>
-                    <span className={`hidden lg:inline cursor-pointer ${showMenu ? 'hidden' : ''}`}>
+                    </motion.span>
+                    <motion.span
+                        className={`hidden lg:inline cursor-pointer ${showMenu ? 'hidden' : ''}`}
+                        initial="hidden"
+                        animate="visible"
+                        variants={graphicsVariants}
+                    >
                         Services
-                    </span>
-                    <span className={`hidden lg:inline cursor-pointer ${showMenu ? 'hidden' : ''}`}>
+                    </motion.span>
+                    <motion.span
+                        className={`hidden lg:inline cursor-pointer ${showMenu ? 'hidden' : ''}`}
+                        initial="hidden"
+                        animate="visible"
+                        variants={graphicsVariants}
+                    >
                         Contact
-                    </span>
-                    <button
+                    </motion.span>
+                    <motion.button
                         className="lg:hidden focus:outline-none"
                         onClick={toggleMenu}
+                        initial="hidden"
+                        animate="visible"
+                        variants={graphicsVariants}
                     >
                         {showMenu ? (
                             <MdClose className="text-white text-xl" />
                         ) : (
                             <TbMenu className="text-white text-xl" />
                         )}
-                    </button>
-                </div>
-            </div>
+                    </motion.button>
+                </motion.div>
+            </motion.div>
             {showMenu && (
                 <div className="lg:hidden h-screen z-[-1] flex flex-col gap-10 items-center justify-center bg-white text-black transition-all duration-500 ease-in">
                     <div className="duration-500 text-3xl font-monument" onClick={toggleMenu}>
